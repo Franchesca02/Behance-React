717d3b33760165c27038e271372643e279f3a09c
franchesca <ihuoma.franchesca@gmail.com>
2021-05-28 19:54:36 -0700

    updated navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,40 +1,23 @@
 import React from './navbar';
-// import { Link } from "react-router-dom";
-import { Menu } from 'react-burger-menu';
-import Navbar from './navbar.css';
+import { Link } from "react-router-dom";
 
 
-export default props => {
+function Navbar() {
     return (
         <div className="nav">
-          <Menu>
 
-          <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/salads">
-        About
-      </a>
-      <a className="menu-item" href="/pizzas">
-        Pricing
-      </a>
-      <a className="menu-item" href="/desserts">
-        Contact
-      </a>
-
- {/* <ul>
- <Link to="/"><li>Home</li>
+ <ul>
+//  <Link to="/"><li>Home</li>
+//         </Link>
+//         <Link to="/About"><li>About</li>
+//         </Link>
+//         <Link to="/pricing"><li>Pricing</li>
         </Link>
-        <Link to="/About"><li>About</li>
-        </Link>
-        <Link to="/pricing"><li>Pricing</li>
-        </Link>
-       <Link to="/Contact">
-       <li>Contact</li>
-       </Link>
+//        <Link to="/Contact">
+//        <li>Contact</li>
+//        </Link>
         <li className="webo">WEBO</li>
-      </ul> */}
-      </Menu>
+</ul> 
       
         </div>
     );
